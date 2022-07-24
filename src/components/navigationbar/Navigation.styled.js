84b83cb254f4ab44212abdb83f3ffef45b2af799
aluku godrick <foreveraluku@gmail.com>
2022-07-25 00:03:57 +0300

    fourth commit

--- src/components/navigationbar/Navigation.styled.js
+++ src/components/navigationbar/Navigation.styled.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  background: var(--icons-pink-color);
+  background: green;
   font-family: var(--poppins);
   font-size: 13px;
   font-weight: 500;
